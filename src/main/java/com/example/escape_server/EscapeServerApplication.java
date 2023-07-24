a10dc5e2809abeaa623235c21549492e46f769c4
hdjung <hdjung@crizen.com>
2023-07-24 10:46:02 +0900

    test

---
+++ src/main/java/com/example/escape_server/EscapeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.escape_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/escape_server/EscapeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.escape_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
